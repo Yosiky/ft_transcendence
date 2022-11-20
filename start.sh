09d7250a8f59fc3cca2c1fc8c28ef1f94d7cfd61
eestelle <yosiky@list.ru>
2022-11-20 20:04:28 +0300

    Init commit

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "lets go"
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker rmi $(docker images -q)
-docker-compose down
-docker volume prune
-docker-compose up --build
